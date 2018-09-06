62b74c31766a2db68268af27287f70218ae63cb7
Krish <krishan.gopal@gmx.com>
2018-09-07 00:26:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krish/krishn/Model/Enclosure.java
@@ -0,0 +1,18 @@
+package com.example.krish.krishn.Model;
+
+public class Enclosure
+{
+    public String link;
+
+    public Enclosure(String link) {
+        this.link = link;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
